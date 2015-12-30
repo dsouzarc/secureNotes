d1bba06fd4d813b781946e972e81a4c179eea3ba
Ryan D'souza <dsouzarc@gmail.com>
2015-12-29 23:52:56 -0600

    First commit

---
+++ SecureNotes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SecureNotes
+//
+//  Created by Ryan D'souza on 12/29/15.
+//  Copyright © 2015 Ryan D'souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SecureNotes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SecureNotes
+//
+//  Created by Ryan D'souza on 12/29/15.
+//  Copyright © 2015 Ryan D'souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
